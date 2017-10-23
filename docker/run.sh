db8e5d42e8df7a49d188b6c7851fa77e09b49ee4
Jean-Philippe Braun <eon@patapon.info>
2017-10-23 13:02:18 +0200

    docker: update checks at runtime

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,7 @@
 
 CASSANDRA_HOST=${1:-localhost}
 
+# Try to update checks
+wget -q https://raw.githubusercontent.com/eonpatapon/contrail-gremlin/master/gremlin-checks/checks.groovy -O checks.groovy
+
 gremlin-dump --cassandra ${CASSANDRA_HOST} dump.json && bin/gremlin.sh -i checks.groovy dump.json
