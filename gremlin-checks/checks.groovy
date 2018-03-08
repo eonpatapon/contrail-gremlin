bebf35afd17399f0195aef59c13f189b7a46cf86
Jean-Philippe Braun <eon@patapon.info>
2018-03-08 10:45:16 +0100

    checks: cleanup

--- gremlin-checks/checks.groovy
+++ gremlin-checks/checks.groovy
@@ -76,64 +76,113 @@ def checkListMap(desc, expr) {
 }
 
 println 'broken references'
-g.V().hasNot('_missing').both().has('_missing').path().map(unfold().map(union(label(), id()).fold()).fold()).each{
-    println '  ' + it[0][0] + '/' + it[0][1] + ' -> ' + it[1][0] + '/' + it[1][1]
+g.V().hasNot('_missing').both().has('_missing').path().map(
+  unfold().map(project("label", "id").by(label).by(id)).fold()
+).each{
+  println '  ' + it[0].get('label') + '/' + it[0].get('id') + ' <-> ' +
+                 it[1].get('label') + '/' + it[1].get('id')
 }
 println ''
 
 check("virtual-network with instance-ip but without any virtual-machine-interface",
-    g.V().hasLabel("virtual_network").not(__.in().hasLabel('virtual_machine_interface')).where(__.in().hasLabel("instance_ip"))
+    g.V().hasLabel("virtual_network")
+         .not(__.in().hasLabel("virtual_machine_interface"))
+         .where(__.in().hasLabel("instance_ip"))
 )
 
 check("virtual-network without routing-instance",
-    g.V().hasLabel('virtual_network').not(__.in().hasLabel('routing_instance'))
+    g.V().hasLabel("virtual_network")
+         .not(__.in().hasLabel("routing_instance"))
+         .hasNot('_missing')
 )
 
 check("virtual-machine-interface without routing-instance",
-    g.V().hasLabel("virtual_machine_interface").not(__.out().hasLabel("routing_instance"))
+    g.V().hasLabel("virtual_machine_interface")
+         .not(out('ref').hasLabel("routing_instance"))
+         .hasNot('_missing')
+)
+
+check("virtual-machine-interface without virtual-network",
+    g.V().hasLabel("virtual_machine_interface")
+         .not(out('ref').hasLabel('virtual_network'))
+         .hasNot('_missing')
 )
 
 check("stale route-targets",
-    g.V().hasLabel("route_target").not(__.in().hasLabel(within("routing_instance", "logical_router")))
+    g.V().hasLabel("route_target")
+         .not(__.in().hasLabel(within("routing_instance", "logical_router")))
 )
 
 check("instance-ip without any instance_ip_address",
-    g.V().hasLabel("instance_ip").not(has("instance_ip_address"))
+    g.V().hasLabel("instance_ip")
+         .hasNot("instance_ip_address")
 )
 
 check("snat without any logical-router",
-    g.V().hasLabel("service_template").has("display_name", "netns-snat-template").in().hasLabel("service_instance").not(__.in().hasLabel("logical_router"))
+    g.V().hasLabel("service_template")
+         .has("display_name", "netns-snat-template")
+         .in().hasLabel("service_instance")
+              .not(__.in().hasLabel("logical_router"))
 )
 
 check("lbaas without any loadbalancer-pool",
-    g.V().hasLabel("service_template").has("display_name", "haproxy-loadbalancer-template").in().hasLabel("service_instance").not(__.in().hasLabel("loadbalancer_pool"))
+    g.V().hasLabel("service_template")
+         .has("display_name", "haproxy-loadbalancer-template")
+         .in().hasLabel("service_instance")
+              .not(__.in().hasLabel("loadbalancer_pool"))
 )
 
 check("lbaas without any virtual-ip",
-    g.V().hasLabel("service_instance").where(__.in().hasLabel("loadbalancer_pool").not(__.in().hasLabel("virtual_ip")))
+    g.V().hasLabel("service_instance")
+         .where(__.in().hasLabel("loadbalancer_pool")
+         .not(__.in().hasLabel("virtual_ip")))
 )
 
 check("routing-instance that doesn't have any route-target (that crashes schema)",
-    g.V().hasLabel("routing_instance").not(has('_missing')).not(has('fq_name', within(["default-domain", "default-project", "ip-fabric", "__default__"], ["default-domain", "default-project", "__link_local__", "__link_local__"]))).not(out().hasLabel("route_target"))
+    g.V().hasLabel("routing_instance")
+         .not(out().hasLabel("route_target"))
+         .hasNot('_missing')
+         .not(has('fq_name', within(["default-domain", "default-project", "ip-fabric", "__default__"],
+                                    ["default-domain", "default-project", "__link_local__", "__link_local__"])))
 )
 
 check("stale access-control-lists",
-    g.V().hasLabel('access_control_list').where(__.outE().hasLabel('parent').count().is(eq(0)))
+    g.V().hasLabel('access_control_list')
+         .not(out('parent'))
 )
 
 checkListMap("virtual-networks duplicate ips",
-    g.V().hasLabel("virtual_network").as('vn').map(union(select('vn'), __.in().hasLabel("instance_ip").has("instance_ip_address").group().by("instance_ip_address").unfold().filter{it.get().value.size > 1}).fold()).filter{it.get().size > 1}
+    g.V().hasLabel("virtual_network").as('vn').map(
+      union(
+        select('vn'),
+        __.in().hasLabel("instance_ip").has("instance_ip_address")
+               .group().by("instance_ip_address").unfold().filter{it.get().value.size > 1}
+      ).fold()
+    ).filter{it.get().size > 1}
 )
 
 checkMap("duplicate floating-ips",
-    g.V().hasLabel(within('floating_ip', 'instance_ip')).or(__.has('floating_ip_address'), __.has('instance_ip_address')).property('ip_address', values('floating_ip_address', 'instance_ip_address')).group().by('ip_address').unfold().filter{it.get().value.size > 1 && it.get().value.findAll{it.label() == "floating_ip"} != []}
+    g.V().hasLabel(within('floating_ip', 'instance_ip'))
+         .or(has('floating_ip_address'), has('instance_ip_address'))
+         .property('ip_address', values('floating_ip_address', 'instance_ip_address'))
+         .group().by('ip_address').unfold()
+         .filter{it.get().value.size > 1 && it.get().value.findAll{it.label() == "floating_ip"} != []}
 )
 
 checkMap("duplicate default security-groups",
-    g.V().hasLabel('project').flatMap(__.in('parent').hasLabel('security_group').has('display_name', 'default').group().by(__.out('parent').hasLabel('project').id()).unfold().filter{it.get().value.size > 1})
+    g.V().hasLabel('project').flatMap(
+      __.in('parent').hasLabel('security_group').has('display_name', 'default')
+                     .group().by(__.out('parent').hasLabel('project').id()).unfold()
+                     .filter{it.get().value.size > 1}
+    )
 )
 
 rtPattern = "target:" + asNumber + ".*"
 check("route target belonging to several tenants (only RT matching the pattern '" + rtPattern + "')",
-    g.V().hasLabel("route_target").where(__.in().hasLabel("routing_instance").out().hasLabel("virtual_network").out().hasLabel("project").dedup().count().is(gt(1))).filter{it.get().value("display_name").matches(rtPattern)}
+    g.V().hasLabel("route_target")
+     .where(
+       __.in().hasLabel("routing_instance")
+              .out('parent').out('parent').dedup().count().is(gt(1))
+     )
+     .filter{it.get().value("display_name").matches(rtPattern)}
 )
