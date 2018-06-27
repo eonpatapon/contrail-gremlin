1b93b166d4dcb4ebd4710631ded213dd9ae136c8
Jean-Philippe Braun <eon@patapon.info>
2018-06-27 14:00:50 +0200

    neutron: don't return deleted vertices in neutron graph

--- resources/scripts/gremlin-contrail.groovy
+++ resources/scripts/gremlin-contrail.groovy
@@ -1,3 +1,3 @@
 def globals = [:]
 
-globals << [g : graph.traversal(), n : graph.traversal().withStrategies(SubgraphStrategy.build().vertices(hasNot('_missing').hasNot('_incomplete')).create())]
+globals << [g : graph.traversal(), n : graph.traversal().withStrategies(SubgraphStrategy.build().vertices(hasNot('_missing').hasNot('_incomplete').has('deleted', 0)).create())]
