fe01f8050932479f59adcd93d98fe62aeab19866
Jean-Philippe Braun <eon@patapon.info>
2018-07-05 12:42:42 +0200

    fsck: add support for testing queries

--- gremlin-fsck/gremlin_fsck/fsck.py
+++ gremlin-fsck/gremlin_fsck/fsck.py
@@ -24,6 +24,8 @@ from .checks import *
 
 avail_checks = [(name, obj) for name, obj in inspect.getmembers(sys.modules[__name__])
                 if inspect.isfunction(obj) and name.startswith('check_')]
+avail_tests = [(name, obj) for name, obj in inspect.getmembers(sys.modules[__name__])
+               if inspect.isfunction(obj) and name.startswith('test_')]
 avail_cleans = [(name, obj) for name, obj in inspect.getmembers(sys.modules[__name__])
                 if inspect.isfunction(obj) and name.startswith('clean_')]
 
@@ -36,6 +38,10 @@ class Fsck(Command):
                     nargs='*', choices=[n[6:] for n, o in avail_checks],
                     default=[n[6:] for n, o in avail_checks],
                     metavar='check')
+    tests = Option(help='Name of tests to run',
+                   nargs='*', choices=[n[5:] for n, o in avail_tests],
+                   default=[],
+                   metavar='test')
     clean = Option(help='Run cleans (default: %(default)s)',
                    action='store_true',
                    default=bool(int(os.environ.get('GREMLIN_FSCK_CLEAN', 0))))
@@ -62,6 +68,14 @@ class Fsck(Command):
             raise CommandError("Can't find %s check method" % name)
         return c
 
+    def _test_by_name(self, name):
+        for n, test in avail_tests:
+            if not name == n[5:]:
+                continue
+            else:
+                c = test
+        return c
+
     def _clean_by_name(self, name):
         c = None
         for n, clean in avail_cleans:
@@ -74,22 +88,25 @@ class Fsck(Command):
             raise CommandError("Can't find %s clean method" % name)
         return c
 
-    def __call__(self, gremlin_server=None, checks=None, clean=False,
+    def __call__(self, gremlin_server=None, checks=None, tests=None, clean=False,
                  loop=False, loop_interval=None, json=False, zk_server=False):
         if clean:
             CommandManager().load_namespace('contrail_api_cli.clean')
         utils.JSON_OUTPUT = json
         utils.ZK_SERVER = zk_server
         self.gremlin_server = gremlin_server
-        if loop is True:
-            self.run_loop(checks, clean, loop_interval)
+        if tests:
+            self.run_tests(tests)
         else:
-            self.run(checks, clean)
+            if loop is True:
+                self.run_loop(checks, clean, loop_interval)
+            else:
+                self.run(checks, clean)
 
     def get_traversal(self):
         graph = Graph()
         try:
-            # take only resources updated at least 5min ago and not deleted
+            # take only non deleted resources
             return graph.traversal().withRemote(
                 DriverRemoteConnection('ws://%s/gremlin' % self.gremlin_server, 'g')
             ).withStrategies(
@@ -102,6 +119,20 @@ class Fsck(Command):
         while True:
             self.run(checks, clean)
             gevent.sleep(loop_interval)
+
+    def run_tests(self, tests):
+        graph = Graph()
+        g = graph.traversal().withRemote(
+            DriverRemoteConnection('ws://%s/gremlin' % self.gremlin_server, 'g')
+        )
+        g.V().drop().iterate()
+        for test_name in tests:
+            test_func = self._test_by_name(test_name)
+            try:
+                test_func(g)
+            except AssertionError as e:
+                utils.log("Test %s failed: %s" % (test_name, e))
+                sys.exit(1)
 
     def run(self, checks, clean):
         g = self.get_traversal()
